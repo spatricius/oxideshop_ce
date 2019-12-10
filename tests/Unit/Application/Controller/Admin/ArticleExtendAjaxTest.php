1c3da6b8ab223f7c8ed42ebab351d352f548f865
Mantas Vaitkunas <manvait@gmail.com>
2019-12-10 16:38:22 +0200

    OXDEV-2685 Fix test for MySQL 8 case
    MySQL 8 returns same case sensitive value as being passed to it.

--- tests/Unit/Application/Controller/Admin/ArticleExtendAjaxTest.php
+++ tests/Unit/Application/Controller/Admin/ArticleExtendAjaxTest.php
@@ -182,7 +182,7 @@ class ArticleExtendAjaxTest extends \OxidTestCase
     public function testGetDataFieldsFalse()
     {
         $oView = oxNew('article_extend_ajax');
-        $this->assertEquals(array(array('FALSE' => 0)), $oView->UNITgetDataFields('select false'));
+        $this->assertEquals(array(array('FALSE' => 0)), $oView->UNITgetDataFields('select FALSE'));
     }
 
     /**
@@ -194,7 +194,7 @@ class ArticleExtendAjaxTest extends \OxidTestCase
     {
         $this->setRequestParameter("oxid", true);
         $oView = oxNew('article_extend_ajax');
-        $this->assertEquals(array(array('FALSE' => 0, '_3' => 0)), $oView->UNITgetDataFields('select false'));
+        $this->assertEquals(array(array('FALSE' => 0, '_3' => 0)), $oView->UNITgetDataFields('select FALSE'));
     }
 
     /**
